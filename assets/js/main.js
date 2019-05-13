dafa82e2397906b8cf7654b561a0298deb6ca3f7
gavbax99 <gav.bax@gmail.com>
2019-05-13 16:19:00 -0700

    comics and nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,4 +16,13 @@ $(document).ready(function(){
         $(".weekly-events-list").slick('slickNext');
     });
 
+    $(".header-bb-logo").on("click", function () {
+        $('html,body').animate({ scrollTop: 0 }, 400);
+    });
+
+    $(".nav-button").on("click", function () {
+        var data = $(this).attr("data-name");
+        $('html,body').animate({ scrollTop: $(`#${data}`).offset().top - 85 }, 400);
+    });
+
 });
