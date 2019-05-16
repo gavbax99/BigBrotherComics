308f64e3755be2fb092346c9ace8a5c748f15a92
gavbax99 <gav.bax@gmail.com>
2019-05-15 17:15:34 -0700

    mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,28 @@ $(document).ready(function(){
         $('html,body').animate({ scrollTop: $(`#${data}`).offset().top - 85 }, 400);
     });
 
+    // ========= INJECTIONS =========
+
+    // Main body injection
+    // $(".bulletin-text-bottom").html("sup<br><br>sup");
+
+    // Title injection
+    // $(".bulletin-title").text("sup<br><br>sup");
+
+    // Author injection
+    // $(".bulletin-author span").text("sup<br><br>sup");
+
+    // Date injection
+    // $(".bulletin-posted-date span").text("sup<br><br>sup");
+
+});
+
+// Preloader fade
+$(window).on("load", function() {
+    $(".preloader").css("height", "0vh");
+    $(".header-preloader").css("opacity", "0");
+    $("body").css("overflow", "auto");
+    setTimeout(function(){ 
+        $(".header-preloader").css("display", "none");
+    }, 1000);
 });
