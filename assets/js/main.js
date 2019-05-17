10748e3bede119546a593bb257bddc020161349d
Gavin <gav.bax@gmail.com>
2019-05-16 17:17:47 -0700

    images

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,31 @@ $(document).ready(function(){
 
     $(".nav-button").on("click", function () {
         var data = $(this).attr("data-name");
+        var navCheck = $(".nav-overlay").css("opacity");
         $('html,body').animate({ scrollTop: $(`#${data}`).offset().top - 85 }, 400);
+        if (navCheck > 0) {
+            $(".ham-img").attr("src", "./assets/images/hamburger_opened.svg");
+            $(".nav-overlay").animate({ opacity: 0 }, 300);
+            setTimeout(function () {
+                $(".nav-overlay").css("display", "none");
+            }, 300);
+        }
+    });
+
+    $(".hamburger").on("click", function() {
+        var check = $(".nav-overlay").css("opacity");
+        if (check == 0) {
+            // If the menu is closed, display it and fade it in
+            $(".nav-overlay").css("display", "flex").animate({ opacity: 1 }, 300);
+            $(".ham-img").attr("src", "./assets/images/hamburger_closed.svg");
+        } else if (check == 1) {
+            // If the menu is opened, fade it out and display:none
+            $(".nav-overlay").animate({ opacity: 0 }, 300);
+            $(".ham-img").attr("src", "./assets/images/hamburger_opened.svg");
+            setTimeout(function () {
+                $(".nav-overlay").css("display", "none");
+            }, 300);
+        }
     });
 
     // ========= INJECTIONS =========
@@ -45,8 +69,9 @@ $(document).ready(function(){
 $(window).on("load", function() {
     $(".preloader").css("height", "0vh");
     $(".header-preloader").css("opacity", "0");
+    $(".hamburger").css("opacity", "1");
     $("body").css("overflow", "auto");
     setTimeout(function(){ 
         $(".header-preloader").css("display", "none");
-    }, 1000);
+    }, 500);
 });
